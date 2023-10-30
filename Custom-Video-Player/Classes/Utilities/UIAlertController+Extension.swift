dc90bd8190010fbfb020284b033e0f4dc48f1015
Ajay <ajayyasodha@gmail.com>
2023-10-30 11:37:08 +0530

    Fixed alert view orientation bug

--- Custom-Video-Player/Classes/Utilities/UIAlertController+Extension.swift
+++ Custom-Video-Player/Classes/Utilities/UIAlertController+Extension.swift
@@ -1,7 +1,8 @@
 import Foundation
 
-// TODO: Fix orientation issue for watch party alert view
-
 extension UIAlertController {
+    @objc func shouldForceLandscape() {
+        //  View controller that response this protocol can rotate ...
+    }
     override open var supportedInterfaceOrientations: UIInterfaceOrientationMask { return .all }
 }
